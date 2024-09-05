ba0caeb2e9b14a7ed9565f10199c266f11fb7267
Vayola <vltgaray@gmail.com>
2024-09-04 20:40:28 -0400

    haciendo cambios de prueba

--- violeta.js
+++ violeta.js
@@ -1,12 +1,12 @@
 const jonatan = required("jonatan.js");
-
+/* modificando mi archivo*/
 function ordenar(){
     let listanumerada = [1,58,93,18,31,8321,63,831,5348,3,18]
     console.log(listanumerada.length);
 
     const listaordenada = [];
-    for (let i = 0; i < listanumerada.length; i++) {
-        for (let j = 0; j < listanumerada.length-1; j++) {
+    for (let i = 0; i < listanumerada.length-1; i++) {
+        for (let j = 0; j < listanumerada.length-1+i; j++) {
             const elemento1 = listanumerada[j];
             const elemento2 = listanumerada[j+1];
             if(elemento1>elemento2){                
