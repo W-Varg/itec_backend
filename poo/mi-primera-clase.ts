bc2f4ecbaaceb99e5832d691f2343f6bc7b1ccd4
Cesar <cesario46@outlook.com>
2024-09-10 21:08:07 -0400

    exponene y raiz cuadrada java

--- poo/mi-primera-clase.ts
+++ poo/mi-primera-clase.ts
@@ -16,7 +16,14 @@ class OperamacionMatematica {
 
 
     // cesar, sacar el exponente, raiz cuadrada
-
+    exp_raiz(){
+        const elevado = Math.pow(this.valor1, this.valor2);
+        const raiz = Math.sqrt(this.valor1);
+        const raiz2 = Math.sqrt(this.valor2);
+        console.log(this.valor1 + "elevado a" + this.valor2 "=" elevado);
+        console.log("La raiz cuadrada de " + this.valor1 + "=" raiz);
+        console.log("La raiz cuadrada de " + this.valor2 + "=" raiz2);
+    }
 
     // marcelo, multiplicar
 
