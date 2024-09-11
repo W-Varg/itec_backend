2912df5737ed8c940617aa21bd15a55d217e7d4e
W-Varg <kryshot05@gmail.com>
2024-09-10 20:43:05 -0400

    se agrego ejercicios

---
+++ poo/mi-primera-clase.ts
@@ -0,0 +1,23 @@
+
+class OperamacionMatematica {
+
+    valor1: number = 0;
+    valor2: number = 0;
+
+    constructor( num1: number,  num2: number) {
+        this.valor1= num1;
+        this.valor2 = num2;
+    }
+
+    // violeta, sumar, restar, multiplicar
+       
+
+    // wilber , dividir a /b, retorne el resto
+
+
+    // cesar, sacar el exponente, raiz cuadrada
+
+
+    // marcelo, multiplicar
+
+}
